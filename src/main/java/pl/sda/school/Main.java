95c70f6bb56256a3297604ff1adbcf42555b3a19
Daniel <kapron.daniel@gmail.com>
2020-03-08 11:27:23 +0100

    School

---
+++ src/main/java/pl/sda/school/Main.java
@@ -0,0 +1,29 @@
+package pl.sda.school;
+
+public class Main {
+
+    //   Stworz klasę School:
+    //                           - String name
+    //                           - String city
+    //            Stworz klase Student:
+    //                           - String name
+    //                           - String lastname
+    //
+    //            Stworz klika szkół i kilkanaście studentów
+    //            Stwórz mapę w której kluczem będzie obiekt szkoly, a wartością lista studentów
+    //
+    //            Map<School, List<Student>>
+    //
+    //            Iteracja po mapie:
+    //                - Szkoła1:
+    //                        - Student1
+    //                        - Student2
+    //                - Szkoła2:
+    //                        -Student1
+    //                        -Student2
+
+    public static void main(String[] args) {
+
+
+    }
+}
