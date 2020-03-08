7905d46cf9f492b9e37f2363eedea757d9320ae4
Daniel <kapron.daniel@gmail.com>
2020-03-08 11:56:23 +0100

    Mapy Zadanie

--- src/main/java/pl/sda/school/Main.java
+++ src/main/java/pl/sda/school/Main.java
@@ -1,5 +1,11 @@
 package pl.sda.school;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     //   Stworz klasę School:
@@ -24,6 +30,59 @@ public class Main {
 
     public static void main(String[] args) {
 
+        School school1 = new School("Szkoła1", "Toruń");
+        School school2 = new School("Szkoła2", "Toruń");
+        School school3 = new School("Szkoła3", "Toruń");
+        School school4 = new School("Szkoła4", "Bydgoszcz");
+
+        Student student1 = new Student("Jan", "Kowalski");
+        Student student2 = new Student("Piotr", "Kowalski");
+        Student student3 = new Student("Wojtek", "Kowalski");
+        Student student4 = new Student("Zbyszek", "Kowalski");
+        Student student5 = new Student("Janusz", "Kowalski");
+        Student student6 = new Student("Jan", "Nowak");
+        Student student7 = new Student("Piotr", "Nowak");
+        Student student8 = new Student("Wojtek", "Nowak");
+        Student student9 = new Student("Zbyszek", "Nowak");
+        Student student10 = new Student("Jan", "Kamiński");
+        Student student11 = new Student("Piotr", "Kamiński");
+        Student student12 = new Student("Wojtek", "Kamiński");
+
+        ArrayList<Student> listaStudentow1 = new ArrayList<Student>();
+        listaStudentow1.add(student1);
+        listaStudentow1.add(student2);
+        listaStudentow1.add(student3);
+
+
+        ArrayList<Student> listaStudentow2 = new ArrayList<Student>();
+        listaStudentow2.add(student4);
+        listaStudentow2.add(student5);
+        listaStudentow2.add(student6);
+
+
+        ArrayList<Student> listaStudentow3 = new ArrayList<Student>();
+        listaStudentow3.add(student7);
+        listaStudentow3.add(student8);
+        listaStudentow3.add(student9);
+
+
+        ArrayList<Student> listaStudentow4 = new ArrayList<Student>();
+        listaStudentow4.add(student10);
+        listaStudentow4.add(student11);
+        listaStudentow4.add(student12);
+
+        Map<School, List<Student>> mapa = new HashMap<School, List<Student>>();
+
+        mapa.put(school1, listaStudentow1);
+        mapa.put(school2, listaStudentow2);
+        mapa.put(school3, listaStudentow3);
+        mapa.put(school4, listaStudentow4);
+
+        for(Map.Entry<School, List<Student>> entry : mapa.entrySet()){
+
+          System.out.println("Szkoła: " + entry.getKey() + " Studenci: " + entry.getValue());
+
+        }
 
     }
 }
---
+++ src/main/java/pl/sda/school/Student.java
@@ -0,0 +1,37 @@
+package pl.sda.school;
+
+public class Student {
+
+    private String name;
+    private String lastName;
+
+    public Student(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
